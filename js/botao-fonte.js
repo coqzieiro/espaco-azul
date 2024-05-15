520461617db84a7f113c267eaaa0b6cf0c197567
Felipe da Costa Coqueiro <felipe.coqueiro@usp.br>
2024-05-15 08:53:30 -0300

    certo

--- js/botao-fonte.js
+++ js/botao-fonte.js
@@ -1,12 +1,22 @@
 document.getElementById('increase-font').addEventListener('click', function() {
-    changeFontSize(2);
+    changeFontSize(1);
 });
 
 document.getElementById('decrease-font').addEventListener('click', function() {
-    changeFontSize(-2);
+    changeFontSize(-1);
 });
 
 function changeFontSize(amount) {
-    var currentFontSize = parseFloat(window.getComputedStyle(document.body, null).getPropertyValue('font-size'));
-    document.body.style.fontSize = (currentFontSize + amount) + 'px';
+    var elements = document.querySelectorAll('*');
+
+    elements.forEach(function(element) {
+        var computedStyle = window.getComputedStyle(element);
+
+        var currentFontSize = parseFloat(computedStyle.fontSize);
+
+        if (!isNaN(currentFontSize)) { 
+            var newFontSize = currentFontSize + amount; 
+            element.style.fontSize = newFontSize + 'px';
+        }
+    });
 }
